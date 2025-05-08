88597be21518295764c40646b84b6181966b1da4
Leandro-Filho <leandro.filho@sou.inteli.edu.br>
2025-05-08 10:45:33 -0300

    mudança no código

--- arquivo.sql
+++ arquivo.sql
@@ -1,13 +1,15 @@
-CREATE TABLE alunos (
-  id SERIAL PRIMARY KEY,
-  nome TEXT NOT NULL,
-  curso TEXT NOT NULL
-);
-
 CREATE TABLE cursos (
   id SERIAL PRIMARY KEY,
   nome TEXT NOT NULL,
   duracao_anos INT
+);
+
+CREATE TABLE alunos (
+  id SERIAL PRIMARY KEY,
+  nome TEXT NOT NULL,
+  turma TEXT NOT NULL,
+  curso TEXT NOT NULL,
+  data_nascimento DATE
 );
 
 CREATE TABLE matriculas (
@@ -17,36 +19,37 @@ CREATE TABLE matriculas (
   data_matricula DATE DEFAULT CURRENT_DATE
 );
 
-INSERT INTO alunos (nome, curso)
-VALUES ('Nicolli','Engenharia de Computação'),
-       ('Paulo', 'Sistema de Informações'),
-       ('Vivian', 'ADM Tech'),
-       ('Hugo', 'ADM Tech'),
-       ('Luana', 'Sistema de Informação'),
-       ('Rafael', 'Engenharia de Computação'),
-       ('Eduardo', 'Engenharia de Computação'),
-       ('Carlos', 'Engenharia de Computação'),
-       ('Felipe', 'Sistema de Informação'),
-       ('Luiz', 'ADM Tech');
+INSERT INTO alunos (nome, turma, curso, data_de_nascimento)
+VALUES
+('Leandro Precaro', '1B', 'Engenharia da Computação', '2006-12-30'),
+('Thulio Bacco', '1B', 'Ciências da Computação', '2006-08-22'),
+('Antônio André', '1A', 'Ciências da Computação', '2005-04-10'),
+('Diego Figueiredo', '1B', 'Ciências da Computação', '2005-11-09'),
+('Eduardo Jesus', '1A', 'Sistema e Informação', '2005-01-30'),
+('Messias Fernandes', '1A', 'Engenharia da Computação', '2006-03-28'),
+('Gabriel Bartmanovi', '1B', 'Ciências da Computação', '2005-06-04'),
+('Vinícius Maciel', '1A', 'Engenharia da Computação', '2007-06-07'),
+('Yuri Boczar', '1A', 'Ciências da Computação', '2007-03-18'),
+('Breno Silva', '1B', 'Ciências da Computação', '2005-12-18');
 
 INSERT INTO cursos (nome, duracao_anos)
-VALUES ('Engenharia de Computação', 4),
-       ('Sistema de Informação', 3),
-       ('ADM Tech', 3);
+VALUES ('Engenharia da Computação', 4),
+       ('Ciências da Computação', 4),
+       ('Sistema e Informação', 4);
 
-INSERT INTO matriculas (aluno_id, curso_id)
+       INSERT INTO matriculas (aluno_id, curso_id)
 VALUES (1, 1),
        (2, 2),
-       (3, 3),
-       (4, 3),
-       (5, 2),
+       (3, 2),
+       (4, 2),
+       (5, 3),
        (6, 1),
-       (7, 1),
+       (7, 2),
        (8, 1),
        (9, 2),
-       (10, 3);
+       (10, 2);
 
-SELECT a.nome AS aluno, c.nome AS curso, m.data_matricula
+       SELECT a.nome AS aluno, c.nome AS curso, m.data_matricula
 FROM matriculas m
-JOIN alunos a ON m.aluno_id = a.id
-JOIN cursos c ON m.curso_id = c.id;
+JOIN alunos a ON m.id_alunos = a.id
+JOIN curso c ON m.id_curso = c.id;
